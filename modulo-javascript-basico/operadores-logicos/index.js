dd494079d1e5a8914d9789c188b78cd7760e6dbc
Felipe Marques <162763543+DevFelipe0993@users.noreply.github.com>
2024-09-01 19:11:45 -0300

    att js basico

---
+++ modulo-javascript-basico/operadores-logicos/index.js
@@ -0,0 +1,19 @@
+/*
+= atribuir
+== comparação
+=== idêntico
+!= diferente
+!== não identico
+< (menor) / > (maior)
+<= menor igual
+>= maior igual
+&& = e
+|| = ou
+! = negando
+
+*/
+
+const a = 3
+const b = "3"
+
+console.log(a !== b)
