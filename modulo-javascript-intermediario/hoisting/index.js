664f53ec55096078bfdf069794c6b459ef828ba9
Felipe Marques <162763543+DevFelipe0993@users.noreply.github.com>
2024-09-06 11:58:18 -0300

    js att

---
+++ modulo-javascript-intermediario/hoisting/index.js
@@ -0,0 +1,6 @@
+let pais = 'Brasil'
+console.log(pais)
+
+// se for declarado depois a variavel ela ira ser lida porem nos bastidores, o JS força uma solicitação de variavel antes de sua chamada.
+
+// Hoisting é o comportamento padrão do JavaScript de mover todas as declarações para o parte superior do escopo atual = içamento
